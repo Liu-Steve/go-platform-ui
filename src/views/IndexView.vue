78de853afb9264c8b45e44cc3799a5c3479a2ebc
zerobut1 <1181948754@qq.com>
2023-11-15 12:20:09 +0800

    IndexView界面优化

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,60 +1,68 @@
 <template>
-    <div>
-        <el-image style="width: 100%;height: 100%;opacity: 0.5;filter: blur(3px);" fit="cover" :src="background"></el-image>
+    <div style="width: 100vw;height: 100vh;overflow:hidden;">
+        <!-- 背景图片 -->
+        <div>
+            <el-image style="width: 100%;height: 100%; ;opacity: 0.5;filter: blur(3px);" fit="cover"
+                :src="background"></el-image>
+        </div>
+
+        <!-- 中央卡片 -->
+        <el-card class="card">
+            <el-row style="height: 100px;">
+                <el-col>
+                    <h1 style="font-size: 48px; text-align: center;">
+                        欢迎来到围棋平台
+                    </h1>
+                </el-col>
+            </el-row>
+
+            <el-row style="margin-top: 10px;">
+                <el-col style="text-align: center;">
+                    <el-text size="large">
+                        您好,XXX
+                        <el-button @click="logout" class="button" type="danger" plain>
+                            退出登录
+                        </el-button>
+                    </el-text>
+                </el-col>
+            </el-row>
+
+            <el-row style="margin-top: 30px;height:250px;">
+                <el-col :span="12">
+                    <el-button @click="createRoom" type="success" plain class="button1">
+                        <div style="width: 100px;height: 150px;">
+                            <el-image :src="home_img"></el-image>
+                            <p style="text-align: center;"> 创建房间</p>
+                        </div>
+                    </el-button>
+                </el-col>
+
+                <el-col :span="12">
+                    <el-button @click="joinRoomDialogVisible.state = true" type="success" plain class="button1">
+                        <div style="width: 100px;height: 150px;">
+                            <el-image :src="plus_img"></el-image>
+                            <p style="text-align: center;"> 加入房间</p>
+                        </div>
+                    </el-button>
+
+                </el-col>
+            </el-row>
+
+        </el-card>
     </div>
 
-    <el-card class="card">
-        <el-row>
-            <el-col>
-                <h1 style="font-size: 48px; text-align: center;">
-                    欢迎来到围棋平台
-                </h1>
-            </el-col>
-        </el-row>
-
-        <el-row>
-            <el-col :span="12">
-                <el-button @click="createRoom" type="success" icon="CaretLeft" plain
-                    style="width:200px;height: 200px;font-size: 24px;">
-                    <div style="width: 100%;height: 100px;text-align: center;">
-                        <el-image :src="plus_img"></el-image>
-                        <p style="text-align: center;">创建房间</p>
-                    </div>
-                </el-button>
-            </el-col>
-
-            <el-col :span="12">
-                <el-button @click="joinRoomDialogVisible.state = true" type="success" plain>
-                    <el-icon>
-                        <Plus />
-                    </el-icon>
-                    加入房间
-                </el-button>
-
-            </el-col>
-        </el-row>
-
-        <el-row style="margin-top: 300px;">
-            <el-col :offset="20" :span="4">
-                <el-button @click="logout" class="button" type="danger" size="large" plain>退出登录</el-button>
-            </el-col>
-        </el-row>
-
-        <div>
-            <!-- 创建房间弹窗 -->
-            <el-dialog title="加入房间" v-model="joinRoomDialogVisible.state" width="40%">
-                <el-form :model="form">
-                    <el-form-item label="房间ID" :label-width="formLabelWidth">
-                        <el-input v-model="form.roomid" autocomplete="off"></el-input>
-                    </el-form-item>
-                </el-form>
-                <div slot="footer" class="dialog-footer">
-                    <el-button @click="joinRoomDialogVisible.state = false">取 消</el-button>
-                    <el-button type="primary" @click="joinRoom">确 定</el-button>
-                </div>
-            </el-dialog>
+    <!-- 创建房间弹窗 -->
+    <el-dialog title="加入房间" v-model="joinRoomDialogVisible.state" width="30%">
+        <el-form :model="form">
+            <el-form-item label="房间ID" :label-width="formLabelWidth">
+                <el-input v-model="form.roomid" autocomplete="off"></el-input>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="joinRoomDialogVisible.state = false">取 消</el-button>
+            <el-button type="primary" @click="joinRoom">确 定</el-button>
         </div>
-    </el-card>
+    </el-dialog>
 </template>
 
 <script setup>
@@ -67,8 +75,8 @@ import { Loading } from 'element-plus/es/components/loading/src/service';
 import { ref } from 'vue';
 import { roomowner, user2 } from "../net/websocket"
 import background from '../assets/img/test.jpg'
-import plus_img from '../assets/img/plus.png'
-import { HomeFilled, Plus } from "@element-plus/icons-vue";
+import home_img from '../assets/img/home.png'
+import plus_img from '../assets/img/add.png'
 
 const router = useRouter();
 
@@ -111,14 +119,22 @@ const joinRoom = () => {
 
 <style scoped>
 .card {
-    width: 1000px;
-    height: 600px;
+    width: 800px;
+    height: 550px;
     position: absolute;
     top: 0;
     bottom: 0;
     left: 0;
     right: 0;
     margin: auto;
-    border-radius: 30px
+    border-radius: 30px;
+}
+
+.button1 {
+    width: 70%;
+    height: 100%;
+    font-size: 24px;
+    display: block;
+    margin: 0 auto;
 }
 </style>
